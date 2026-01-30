6c5a796e1354ab925927e3cb2ab45896cd4239cc
kat22 <kat22744@gmail.com>
2026-01-30 16:58:14 +0100

    add create routes

--- app.py
+++ app.py
@@ -6,7 +6,16 @@ import json
 import dateutil.parser
 import babel
 from datetime import datetime
-from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask import (
+    Flask,
+    abort,
+    render_template,
+    request,
+    Response,
+    flash,
+    redirect,
+    url_for,
+)
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -259,11 +268,38 @@ def create_venue_form():
 
 @app.route("/venues/create", methods=["POST"])
 def create_venue_submission():
-    # TODO: insert form data as a new Venue record in the db, instead
-    # TODO: modify data to be the data object returned from db insertion
-
-    # on successful db insert, flash success
-    flash("Venue " + request.form["name"] + " was successfully listed!")
+    print(request.form)
+    error = False
+    body = {}
+    try:
+        venue = Venue(
+            name=request.form["name"],
+            city=request.form["city"],
+            state=request.form["state"],
+            address=request.form["address"],
+            phone=request.form["phone"],
+            genres=request.form.getlist("genres"),
+            facebook_link=request.form["facebook_link"],
+            image_link=request.form["image_link"],
+            website_link=request.form["website_link"],
+            is_looking_for_talent=True if "seeking_talent" in request.form else False,
+            seeking_description=request.form["seeking_description"],
+        )
+        db.session.add(venue)
+        db.session.commit()
+    except Exception as e:
+        error = True
+        db.session.rollback()
+        print(f"Error occurred: {e}")
+    finally:
+        db.session.close()
+    if error:
+        flash(
+            "An error occurred. Venue " + request.form["name"] + " could not be listed."
+        )
+        abort(400)
+    else:
+        flash("Venue " + request.form["name"] + " was successfully listed!")
     # TODO: on unsuccessful db insert, flash an error instead.
     # e.g., flash('An error occurred. Venue ' + data.name + ' could not be listed.')
     # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
@@ -467,6 +503,36 @@ def create_artist_form():
 
 @app.route("/artists/create", methods=["POST"])
 def create_artist_submission():
+    print(request.form)
+    error = False
+    try:
+        artist = Artist(
+            name=request.form["name"],
+            city=request.form["city"],
+            state=request.form["state"],
+            phone=request.form["phone"],
+            genres=request.form.getlist("genres"),
+            facebook_link=request.form["facebook_link"],
+            image_link=request.form["image_link"],
+            website_link=request.form["website_link"],
+            is_looking_for_venues=True if "seeking_venue" in request.form else False,
+            seeking_description=request.form["seeking_description"],
+        )
+        db.session.add(artist)
+        db.session.commit()
+    except Exception as e:
+        error = True
+        db.session.rollback()
+        print(f"Error occurred: {e}")
+    finally:
+        db.session.close()
+    if error:
+        flash(
+            "An error occurred. Artist "
+            + request.form["name"]
+            + " could not be listed."
+        )
+        abort(400)
     # called upon submitting the new artist listing form
     # TODO: insert form data as a new Venue record in the db, instead
     # TODO: modify data to be the data object returned from db insertion
@@ -561,6 +627,25 @@ def create_shows():
 
 @app.route("/shows/create", methods=["POST"])
 def create_show_submission():
+    print(request.form)
+    error = False
+    try:
+        show = shows_list.insert().values(
+            venue_id=request.form["venue_id"],
+            artist_id=request.form["artist_id"],
+            start_time=request.form["start_time"],
+        )
+        db.session.execute(show)
+        db.session.commit()
+    except Exception as e:
+        error = True
+        db.session.rollback()
+        print(f"Error occurred: {e}")
+    finally:
+        db.session.close()
+    if error:
+        flash("An error occurred. Show could not be listed.")
+        abort(400)
     # called to create new shows in the db, upon submitting new show listing form
     # TODO: insert form data as a new Show record in the db, instead
 
